82f89298b4f024fa48fa68609a6053b762589109
Ник Николос <nic23173@gmail.com>
2021-05-16 17:44:22 +0300

    structure: test scss

---
+++ src/Homepage.component.jsx
@@ -0,0 +1,14 @@
+export default function HomePage() {
+    return (
+      <div className='homepage'>
+        <div className='directory-menu'>
+          <div className='menu-item'>
+            <div className='content'>
+              <h2 className='title'>HATS</h2>
+              <span className='subtitle'>SHOP NOW</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
