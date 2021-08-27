eb45ddf32d043fd19bc46de459035d33004bdbc5
Ник Николос <nic23173@gmail.com>
2021-08-27 08:24:01 +0300

    updating our app with a shop page, collection preview component and collection item component

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CollectionItem } from '../collection-item/collection-item.component';
 import './preview-collection.styles.scss';
 
 export function CollectionPreview({ title, items }) {
@@ -6,8 +7,10 @@ export function CollectionPreview({ title, items }) {
     <div className='collection-preview'>
       <h2 className='title'>{title.toUpperCase()}</h2>
       <div className='preview'>
-        {items.filter((_, index) => index < 4).map((item) => (
-          <div key={item.id}>{item.name}</div>
+        {items
+          .filter((_, index) => index < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
         ))}
       </div>
     </div>
